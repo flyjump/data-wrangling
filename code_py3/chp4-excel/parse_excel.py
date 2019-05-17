83ad7a7f6e078f21a98f2729afda8f661beef552
cool <cool_kj@126.com>
2019-05-17 19:26:29 +0800

    Python3 excel

---
+++ code_py3/chp4-excel/parse_excel.py
@@ -0,0 +1,39 @@
+"""
+    This is a script to parse child labor and child marriage data.
+    The excel file used in this script can be found here:
+        http://www.unicef.org/sowc2014/numbers/
+"""
+
+import xlrd
+import pprint
+
+book = xlrd.open_workbook("../../data/chp4/SOWC 2014 Stat Tables_Table 9.xlsx")
+
+sheet = book.sheet_by_name("Table 9 ")
+
+data = {}
+for i in range(14, sheet.nrows):
+
+    # Start at 14th row, because that is where the countries begin
+    row = sheet.row_values(i)
+
+    country = row[1]
+
+    data[country] = {
+        'child_labor': {
+            'total': [row[4], row[5]],
+            'male': [row[6], row[7]],
+            'female': [row[8], row[9]],
+        },
+        'child_marriage': {
+            'married_by_15': [row[10], row[11]],
+            'married_by_18': [row[12], row[13]],
+        }
+    }
+
+    if country == "Zimbabwe":
+        break
+
+pprint.pprint(data)
+# print sheet names the result is a list
+print(book.sheet_names())
